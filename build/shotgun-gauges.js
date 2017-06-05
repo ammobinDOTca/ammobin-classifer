4bcb6cb0028fcb591e697cbbbdf1612d3039ef9d
Simon R <nexus-uw@users.noreply.github.com>
2017-06-04 22:29:02 -0400

    added more shotgun classifications
    - catches ALOT of miss categorizations

--- build/shotgun-gauges.js
+++ build/shotgun-gauges.js
@@ -5,25 +5,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 exports.shotgunGauges = [
     [
-        '10 GA'
+        '10 GA',
+        '10GA'
     ],
     [
-        '12 GA'
+        '12 GA',
+        '12GA'
     ],
     [
-        '14 GA'
+        '14 GA',
+        '14GA'
     ],
     [
-        '16 GA'
+        '16 GA',
+        '16GA'
     ],
     [
-        '20 GA'
+        '20 GA',
+        '20GA'
     ],
     [
-        '28 GA'
+        '28 GA',
+        '28GA'
     ],
     [
         '.410 GA',
+        '.410GA',
+        '410GA',
         '410'
     ]
 ];
