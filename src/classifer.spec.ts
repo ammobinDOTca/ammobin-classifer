2fac5ede4585c8e8868539db9adde9a8f6afb0c4
Simon Ramsay <nexus-uw@users.noreply.github.com>
2017-07-15 12:32:18 -0400

    merged nato and civilian calibres (#15)

--- src/classifer.spec.ts
+++ src/classifer.spec.ts
@@ -11,13 +11,13 @@ describe('classifer()', () => {
       ['Prvi Partizan Ammunition 222 Remington 50 Grain Soft Point', '.222 remington'],
       [' PRVI Ammunition .380 ACP 94gr JHP - Box of 50 ', '.380 acp'],
       ['Prvi Partizan (PPU) Rifle Ammo - 6.5x52mm Carcano, 139Gr, FMJ, 20rds Box ', '6.5x52mm carcano'],
-      [' Norinco 7.62x51, 145 Grain FMJ, 1000rd Crate ', '7.62 nato'],
-      ['Hirtenberger 7.62x51 Nato 240 Battle Pack, Berdan Primed ', '7.62 nato'],
+      [' Norinco 7.62x51, 145 Grain FMJ, 1000rd Crate ', '.308 / 7.62 nato'],
+      ['Hirtenberger 7.62x51 Nato 240 Battle Pack, Berdan Primed ', '.308 / 7.62 nato'],
       ['Sako Super Hammerhead 300 Win. Mag. 180gr Bonded Soft Point Boat Tail ', '.300 winchester short magnum'],
       [' FEDERAL 300WIN 180GR SPEER HOT CORE SP POWERSHOK ', '.300 winchester short magnum'],
       [' CCI 9mm Luger, 115 Grain FMJ , box of 50 ', '9mm'],
       [' BROWNING BPT 9mm Luger, 115 Grain FMJ , box of 50 ', '9mm'],
-      ['Hirtenberger 7.62x51 Nato Boxer, Primed', '7.62 nato']
+      ['Hirtenberger 7.62x51 Nato Boxer, Primed', '.308 / 7.62 nato']
     ].forEach(t => it(`"${t[0]} => ${t[1]}`, () =>
       expect(classify(centerFireCalibres, t[0]))
         .toEqual(t[1])));
