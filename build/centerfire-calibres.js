6eb689b91e29d35f6fa9488db1d53329abcee061
Simon R <nexus-uw@users.noreply.github.com>
2019-02-24 22:28:42 -0800

    added new calibres + fixed formatting

--- build/centerfire-calibres.js
+++ build/centerfire-calibres.js
@@ -1,9 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
 /**
  * known centerfire calibres
  */
 exports.centerFireCalibres = [
+<<<<<<< HEAD
     ['.357 mag', '357mag'],
     ['.357 sig', '357sig'],
     ['.223 / 5.56 nato',
@@ -95,3 +96,68 @@ exports.centerFireCalibres = [
     ['5.45x39'],
 ];
 //# sourceMappingURL=centerfire-calibres.js.map
+=======
+  ['.357 mag', '357mag'],
+  ['.223 / 5.56 nato', '5.56', '5.56x45', '.223 remington', '223', '556 rem'],
+  ['7.62 x 25 tokarev', '7.62x25'],
+  ['.38 special', '38spl', '38special'],
+  ['.308 / 7.62 nato', '7.62x51', '.308 winchester', '308win', '.308'],
+  ['.45 acp', '45acp', '45auto'],
+  ['.45 gap', '45g.a.p.', '45gap'],
+  ['7mm remington magnum', '7mmremingtonmag'],
+  ['.32 auto', '32auto'],
+  ['.380 acp', '380auto', '9x17', '380acp'],
+  ['.40 s&w', '40s&w', '40 sw'],
+  ['7.5 x 55mm swiss', '7.5x55'],
+  ['9mm', '9x19'],
+  ['7.62 x 54mmr', '7.62x54'],
+  ['7.62 x 39mm', '7.62x39', '762x39'],
+  ['338 lapua magnum', '8.6x70mm', '8.58x70mm', '338lapua'],
+  ['.338 winchester magnum', '338winchestermagnum', '338win'],
+  ['6.5mm creedmoor', '6.5creedmoor', '6.5cm'],
+  ['.460 s&w magnum', '.460s&w'],
+  ['.500 s&w magnum', '500s&wmag', '12.7x41mmsr'],
+  ['.44 magnum', '44remingtonmagnum', '44mag', '44remmag'],
+  ['.25-06 remington', '25-06'],
+  ['.30 carbine', '30carbine', '7.62×33mm'],
+  ['.30-06 springfield', '30-06sprg', '30-06'],
+  ['.30-30 winchester', '7.8x51r', '30winchester', 'thirty-thirty', '30-30', '3030win'],
+  ['.303 british', '303british', '7.7x56mmr'],
+  ['.270 win', '270win'],
+  ['.300 winchester short magnum', '300wsm'],
+  ['.300 winchester magnum', '300win', '300wn'],
+  ['6.5x55mm', '6.5x55'],
+  ['.240 Weatherby Magnum', '240weatherby', '240wby'],
+  ['.270 weatherby magnum', '270wby', '270weatherby'],
+  ['.300 weatherby magnum', '300weatherby', '300wby'],
+  ['.340 weatherby magnum', '340wby', '340weatherby'],
+  ['.30-378 weatherby magnum', '30-378wby', '30-378weatherby'],
+  ['.338-378 weatherby magnum', '338-378wby', '.338-378weatherby'],
+  ['.50 bmg', '50bmg', '50browningmachinegun', '12.7x99mmnato', '50browning'],
+  ['.300 aac blackout', '300blk', '7.62x35mm', '300aac', '300blackout'],
+  ['7.92x57mmmauser', '8mmmauser', '8x57', '8mm'],
+  ['5.6x52mmr', '5.6x52r', '22savagehigh-power'],
+  ['.243 winchester', '6x52mm', '243win'],
+  ['.204 ruger', '204ruger'],
+  ['.22-250 remington', '22-250'],
+  ['.45 colt', '45longcolt', '45lc', '11.48x33mmr', '45colt'],
+  ['7mm-08 remington', '7mm-08', '7mmrem'],
+  ['fn 5.7x28mm', '5.7x28mm'],
+  ['10mm auto', '10mm', '10x25mm'],
+  ['9.3x62mm', '9.3x62'],
+  ['.444 marlin', '444marlin'],
+  ['.454 casull', '454casull'],
+  ['6.5x52mm carcano', '6.5x52'],
+  ['7x57mm mauser', '7x57', '7mm'],
+  ['.32 winchester special', '32ws', '32winchesterspecial'],
+  ['.45-70', '45-70'],
+  ['.50 AE', '50ae', '50action'],
+  ['.222 remington', '222'],
+  ['.375 ruger', '375ruger'],
+  ['.270 winchester short magnum', '270winchestershortmagnum', '270wsm'],
+  ['5.45x39'],
+  ['.416 Rigby', '416Rigby', '10.57x74'],
+  ['6.5 PRC', '6.5prc'],
+]
+//# sourceMappingURL=centerfire-calibres.js.map
+>>>>>>> added new calibres + fixed formatting
--- src/classifer.spec.ts
+++ src/classifer.spec.ts
@@ -1,20 +1,18 @@
-import { classify } from './classifier';
-import { shotgunGauges } from './shotgun-gauges';
-import { centerFireCalibres } from './centerfire-calibres';
-import { rimfireCalibres } from './rimfire-calibres';
-import { brands } from './brands';
+import { classify } from './classifier'
+import { shotgunGauges } from './shotgun-gauges'
+import { centerFireCalibres } from './centerfire-calibres'
+import { rimfireCalibres } from './rimfire-calibres'
+import { brands } from './brands'
 
 describe('classifer()', () => {
-  describe('centerfire', () => {
+  describe('centerfire', () =>
     [
       ['Federal PowerShok 5RND Box 3" 1.25oz Slug 12GA', '12 ga'],
       ['Federal 12-Gauge 2-3/4-in Rifled Slug ', '12 ga'],
-    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () =>
-      expect(classify(shotgunGauges, t[0]))
-        .toEqual(t[1])));
-  });
+      ['Winchester Super-Target 20 Gauge #8, 2-3/4", Case of 250 #TRGT208-CASE', '20 ga'],
+    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () => expect(classify(shotgunGauges, t[0])).toEqual(t[1]))))
 
-  describe('centerfire', () => {
+  describe('centerfire', () =>
     [
       ['Prvi Partizan Ammunition 222 Remington 50 Grain Soft Point', '.222 remington'],
       [' PRVI Ammunition .380 ACP 94gr JHP - Box of 50 ', '.380 acp'],
@@ -30,23 +28,17 @@ describe('classifer()', () => {
       ['PRVI Ammunition 6.5x52 Carcano 139gr FMJ-BT - Box of 20 ', '6.5x52mm carcano'],
       ['Sellier & Bellot 300 AAC Blackout Ammunition, 124gr FMJ. ', '.300 aac blackout'],
       [' Sellier & Bellot 300 Blackout, 200gr FMJ Subsonic, Box of 20 ', '.300 aac blackout'],
-      ['FEDERAL POWER SHOK 30 CARBINE 110 GR 30CA', '.30 carbine']
-    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () =>
-      expect(classify(centerFireCalibres, t[0]))
-        .toEqual(t[1])));
-  });
+      ['FEDERAL POWER SHOK 30 CARBINE 110 GR 30CA', '.30 carbine'],
+    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () => expect(classify(centerFireCalibres, t[0])).toEqual(t[1]))))
 
-  describe('rimfire', () => {
+  describe('rimfire', () =>
     [
       [' Winchester 22 wmr 30 gr vmax ', '.22 wmr'],
       ['CCI 22LR Standard Velocity Lead RN 40gr 5000/case ', '.22 lr'],
       [' CCI 22LR MINI-MAG, HIGH-VELOCITY, 40Gr RN 5000/Case ', '.22 lr'],
       ['Aguila 22LR Interceptor Solid Point 40gr 1470FPS Ammunition ', '.22 lr'],
-    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () =>
-      expect(classify(rimfireCalibres, t[0]))
-        .toEqual(t[1])));
-  });
-  describe('brands', () => {
+    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () => expect(classify(rimfireCalibres, t[0])).toEqual(t[1]))))
+  describe('brands', () =>
     [
       [' Glock G22 Gen4 + 500 Round of PMC 40 S&W 180gr COMBO ', 'pmc'],
       ['IMI 5.56x45 NATO 62gr M855 Green Tip FMJ- Box of 30', 'imi'],
@@ -59,8 +51,5 @@ describe('classifer()', () => {
       [' Federal 223 Remington 55gr FMJ - 1000 Rounds ', 'federal'],
       [' REMINGTON 223 REMINGTON 55 PSP ', 'remington'],
       ['fiocchi ammunition 223 remington fmj boat tail 55 grain box 50 fio 223a ', 'fiocchi'],
-    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () =>
-      expect(classify(brands, t[0], true))
-        .toEqual(t[1])));
-  });
-});
+    ].forEach(t => it(`"${t[0]} => ${t[1]}`, () => expect(classify(brands, t[0], true)).toEqual(t[1]))))
+})
