f6e969677fa968fbfee74213ab24e39d7b05ec40
nexus-uw <contact@ammobin.ca>
2019-03-25 05:05:33 +0000

    v0.0.34

--- build/rimfire-calibres.js
+++ build/rimfire-calibres.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * know rimfire calibres
  */
 exports.rimfireCalibres = [
-    ['.22 lr', '22lr', '22longrifle', 'longrifle', '22'],
-    ['.22 wmr', '22wmr', '22winchestermagnum', '22magnum', '22winmag', '22mag'],
+    ['.22 wmr', '22wmr', '22winchestermagnum', '22magnum', '22winmag', '22mag', '22wrf'],
+    ['.22 lr', '22lr', '22longrifle', 'longrifle', '22' /*default all 22 to lr*/],
     ['.17 hmr', '17hmr', '17hornadymagnumrimfire'],
-    ['.17 wsm', '17wsm']
+    ['.17 wsm', '17wsm'],
 ];
 //# sourceMappingURL=rimfire-calibres.js.map
